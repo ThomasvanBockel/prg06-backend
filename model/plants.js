fa9b053f5c3f87e7ff2daf523d26b2cef6cb2f8e
Thomas van Bockel <Tnvanbockel@gmail.com>
2026-01-19 12:47:32 +0100

    made plants database

---
+++ model/plants.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+
+const plantsSchema = new mongoose.Schema({
+        name:        {type: String, required: true},
+        description: {type: String, required: true},
+        type:        {type: String, required: true},
+    },
+
+    {
+        toJSON: {
+            virtuals: true,
+            versionKey: false,
+            transform: (doc, ret) => {
+                ret._links = {
+                    self: {
+                        href: `${process.env.BASE_URI}${ret._id}`,
+                    },
+                    collection: {
+                        href: `${process.env.BASE_URI}`,
+                    },
+                };
+
+                delete ret._id;
+            },
+        },
+
+    }
+);
+
+const Plant = mongoose.model("Plant", plantsSchema);
+
+export default Plant;
